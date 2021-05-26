d8a5d7cd74a32d6d79b509495eca0312ab16f3b4
NicFragale <nic.fragale@netfoundry.io>
2021-05-26 14:35:07 -0600

    Update to allow PIPE/FIFO on Docker

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,17 +1,19 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 function alldone() {
     # send SIGINT to ziti-tunnel to trigger a cleanup of iptables mangle rules
-    kill -INT $ZITI_EDGE_TUNNEL_PID
+    kill -INT $ZITI_TUNNEL_PID
     # let entrypoint script exit after ziti-tunnel PID
-    wait $ZITI_EDGE_TUNNEL_PID
+    wait $ZITI_TUNNEL_PID
 }
 trap alldone exit
 
 # Ensure that ziti-tunnel's identity is stored on a volume
 # so we don't throw away the one-time enrollment token
 
-persisted_dir="/ziti-edge-tunnel"
+persisted_dir="/netfoundry"
 wc_lines=$(df / "${persisted_dir}" 2> /dev/null | uniq | wc -l)
 if [ "${wc_lines}" != "3" ]; then
     echo "ERROR: please run this image with ${persisted_dir} mounted on a volume"
@@ -32,25 +34,50 @@ fi
 
 json="${persisted_dir}/${NF_REG_NAME}.json"
 if [ ! -f "${json}" ]; then
-    echo "identity configuration ${json} does not exist"
-    for dir in "/var/run/secrets/netfoundry.io/enrollment-token" "${persisted_dir}"; do
-        _jwt="${dir}/${NF_REG_NAME}.jwt"
-        echo "looking for ${_jwt}"
-        if [ -f "${_jwt}" ]; then
-            jwt="${_jwt}"
-            break
-        fi
+    echo "WARN: identity configuration ${json} does not exist" >&2
+    for dir in  "/var/run/secrets/kubernetes.io/enrollment-token" \
+                "/enrollment-token" \
+                "${persisted_dir}";
+        do
+            [[ -d ${dir} ]] || {
+                echo "INFO: ${dir} is not a directory"
+                continue
+            }
+            _jwt="${dir}/${NF_REG_NAME}.jwt"
+            echo "INFO: looking for ${_jwt}"
+            if [ -f "${_jwt}" ]; then
+                jwtfile="${_jwt}"
+                break
+            elif [ -p "${_jwt}" ]; then
+                read -rt1 jwtstring < "${_jwt}"
+                break                
+            else
+                echo "WARN: failed to find ${_jwt} in ${dir}" >&2
+            fi
     done
-    if [ -z "${jwt}" ]; then
-        echo "ERROR: ${NF_REG_NAME}.jwt was not found in the expected locations"
+    if [ -z "${jwt:-}" ]; then
+        echo "ERROR: ${NF_REG_NAME}.jwt was not found in the expected locations" >&2
         exit 1
     fi
-    echo "enrolling ${jwt}"
-    ziti-edge-tunnel enroll --jwt "${jwt}" --identity "${json}"
+    echo "INFO: enrolling ${NF_REG_NAME}"
+    ziti-tunnel enroll --jwt <(cat<<<"${jwtstring:-$(echo "${jwtstdin:-$(cat<"${jwtfile}")}")}") --out "${json}"
 fi
 
-echo "running ziti-edge-tunnel"
+echo "INFO: probing iptables"
+if iptables -t mangle -S --wait 2>&1 | grep -q "iptables-legacy tables present"; then
+    for LEGACY in {ip{,6},eb,arp}tables; do
+        if which ${LEGACY}-legacy &>/dev/null; then
+            echo "INFO: updating $LEGACY alternative to ${LEGACY}-legacy"
+            update-alternatives --set $LEGACY $(which ${LEGACY}-legacy)
+        else
+            echo "WARN: not updating $LEGACY alternative to ${LEGACY}-legacy" >&2
+        fi
+    done
+fi
+
+# optionally run an alternative shell CMD
+echo "running ziti-tunnel"
 set -x
-ziti-edge-tunnel run --identity "${json}" "${@}" &
-ZITI_EDGE_TUNNEL_PID=$!
-wait $ZITI_EDGE_TUNNEL_PID
+ziti-tunnel -i "${json}" "${@}" &
+ZITI_TUNNEL_PID=$!
+wait $ZITI_TUNNEL_PID
